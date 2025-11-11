049a900a0a2b5588bc8a4f97cbf7bed1dd07706e
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-11 17:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const age = document.getElementById("age").value.trim();
+	const name = document.getElementById("name").value.trim();
+
+	if(age === "" || name === "") {
+		alert("Please enter valid details.");
+		return;
+	}
+
+	const prom = new Promise((resolve, reject) => {
+		if(age > 18) {
+			setTimeout(() => {
+				resolve("Welcome, . You can vote")
+			}, 4000)
+		} else{
+			setTimeout(() => {
+				resolve("Oh sorry . You aren't old enough")
+			}, 4000)
+		}
+	})
+
+	prom
+		.then((data) => alert(data))
+		.catch((err) => alert(err))
+	
+	
+})
